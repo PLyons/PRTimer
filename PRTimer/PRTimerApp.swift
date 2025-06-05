ee3e5350de7c1d6f329175048cd193308c80dc0a
PLyons <pel1007@yahoo.com>
2025-06-05 12:39:44 -0400

    Initial Commit

---
+++ PRTimer/PRTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PRTimerApp.swift
+//  PRTimer
+//
+//  Created by Paul Lyons on 6/5/25.
+//
+
+import SwiftUI
+
+@main
+struct PRTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PRTimerTests/PRTimerTests.swift
@@ -0,0 +1,17 @@
+//
+//  PRTimerTests.swift
+//  PRTimerTests
+//
+//  Created by Paul Lyons on 6/5/25.
+//
+
+import Testing
+@testable import PRTimer
+
+struct PRTimerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
